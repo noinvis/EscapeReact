81723ca708a29c826208cefc728b2105f5812223
noinvis <lazizbek.sn@gmail.com>
2025-07-06 22:56:35 +0500

    Error clear

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.scss'
 import Featured from './components/featured/Featured'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
-import Recent from './components/recent/recent'
+import Recent from './components/recent/Recent'
 import Type from './components/type/Type'
 
 function App() {
